0c8e3f6fe0eea87f2cc7384c5ffed7d21f668921
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-04 18:17:22 -0500

    added animation effects to tabs

--- _site/js/about.js
+++ _site/js/about.js
@@ -1,28 +1,47 @@
 $(function(){
   $(".tab-btns").on('click', 'a', function(e){
-
     // grab item clicked on
-    var $active = $(this);
+    var $this = $(this);
 
     // swap classes and display content
-    swapClass($active.attr('href'));
+    swapClass($this.attr('href'), $this);
 
     e.preventDefault();
   });
 });
 
-function swapClass(clickedClass) {
+function swapClass(clickedTabLink, clickedTab) {
 
-  // store href value to use for conditional step
+  // store values for later use in conditionals and add/remove classes
   var story = $('.tab-btns li:first-child a').attr('href');
+  var $longs = $('#long');
+  var $shorts = $('#short');
+  var $tabButtons = $(".tab-btns");
 
-  // if short story was clicked add class active to short
-  // else add inactive class to short and active to long
-  if(clickedClass === story) {
-    $('#short').removeClass('inactive').addClass('active');
-    $('#long').removeClass('active').addClass('inactive');
+  // if short story was clicked add class show to short and add hide to long
+  // else reverse the above
+  // also add an show tab
+  if(clickedTabLink === story) {
+
+    // change the tabs
+    $tabButtons.find("li a.active").removeClass('active');
+    clickedTab.addClass('active')
+
+    // change the content
+    $longs.fadeOut('fast', function() {
+      $longs.removeClass('show').addClass('hide');
+      $shorts.addClass('show').removeClass('hide');
+    }).fadeIn();
   } else {
-    $('#long').removeClass('inactive').addClass('active');
-    $('#short').removeClass('active').addClass('inactive');
+
+    // change the tabs
+    $tabButtons.find("li a.active").removeClass('active');
+    clickedTab.addClass('active')
+
+    // change the content
+    $shorts.fadeOut('fast', function() {
+      $shorts.addClass('hide').removeClass('show');
+      $longs.removeClass('hide').addClass('show');
+    }).fadeIn();
   }
 }
