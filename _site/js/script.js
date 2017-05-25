6affe40974827f2106e75159f0f7c29161f49e3f
Pratik Shah <pratik.t.shah@gmail.com>
2017-05-25 09:33:30 -0500

    Remove skills

--- _site/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  (".tab-btns a").on('click', 'a', function(e){
-    e.preventDefault();
-
-    // grab item clicked on
-    $tabClicked = $(this);
-    console.log($tabClicked);
-  });
-});
