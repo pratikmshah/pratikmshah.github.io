7e8078dc8f0b9a532cfba4da1b131881642405e9
Pratik Shah <pratik.t.shah@gmail.com>
2017-12-31 21:46:19 -0600

    Change search from yahoo to finviz

--- tradelytics/scripts/script.js
+++ tradelytics/scripts/script.js
@@ -15,10 +15,9 @@ $(document).ready(function() {
       e.preventDefault();
       // variables
       var ticker = $("#ticker").val();
-      var YHOO_LINK = "https://finance.yahoo.com/chart/";
-      var YHOO_PARAMS = "#eyJzaG93QXJlYSI6ZmFsc2UsInNob3dMaW5lIjpmYWxzZSwibXVsdGlDb2xvckxpbmUiOmZhbHNlLCJzaG93Q2FuZGxlIjp0cnVlLCJib2xsaW5nZXJVcHBlckNvbG9yIjoiI2UyMDA4MSIsImJvbGxpbmdlckxvd2VyQ29sb3IiOiIjOTU1MmZmIiwic2hvd0VtYSI6dHJ1ZSwiZW1hQ29sb3JzIjoiI2YwMTI2ZiwjMWE5YWY5LCMxYWM1NjciLCJlbWFQZXJpb2RzIjoiNTAsMjAsMTAiLCJlbWFXaWR0aHMiOiIyLDEsMSIsImVtYUdob3N0aW5nIjoiMCwwLDAiLCJtZmlMaW5lQ29sb3IiOiIjNDVlM2ZmIiwibWFjZERpdmVyZ2VuY2VDb2xvciI6IiNmZjdiMTIiLCJtYWNkTWFjZENvbG9yIjoiIzc4N2Q4MiIsIm1hY2RTaWduYWxDb2xvciI6IiMwMDAwMDAiLCJyc2lMaW5lQ29sb3IiOiIjZmZiNzAwIiwic3RvY2hLTGluZUNvbG9yIjoiI2ZmYjcwMCIsInN0b2NoRExpbmVDb2xvciI6IiM0NWUzZmYiLCJsaW5lVHlwZSI6ImNhbmRsZSIsInJhbmdlIjoiM21vIn0%3D";
+      var FINVIZ_LINK = "https://finviz.com/quote.ashx?t=";
 
-      window.open(YHOO_LINK + ticker + YHOO_PARAMS);
+      window.open(FINVIZ_LINK + ticker);
 
       $("#ticker").val('');
   });
