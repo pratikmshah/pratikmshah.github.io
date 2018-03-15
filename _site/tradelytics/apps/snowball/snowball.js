27fc8fbbf4aa32cc1b4aab674ab4f7814678567c
Pratik Shah <pratik.t.shah@gmail.com>
2018-03-15 17:58:06 -0500

    construct chart urls

--- _site/tradelytics/apps/snowball/snowball.js
+++ _site/tradelytics/apps/snowball/snowball.js
@@ -3,14 +3,25 @@ $(function() {
   // execute and return charts for stock tickers from text box
   $('#run').on('click', function() {
     var tickers = parseTickerSymbols();
+    tickers = createUrls(tickers);
+
   });
 
   // parse user text box symbols
   function parseTickerSymbols() {
-    const URL_BASE = "https://finviz.com/quote.ashx?";
-    const URL_CHART_SPEC = "&ty=c&p=d&b=1";
-    var $tickers = $('.input-group input').val();
+    return  $('.input-group input').val().split(',');
+  }
 
+  // construct chart urls
+  function createUrls(arr) {
+    const URL_BASE = "https://finviz.com/chart.ashx?t=";
+    const URL_CHART_SPEC = "&ty=c&ta=1&p=d&s=l";
+
+    for(var i = 0; i < arr.length; i++) {
+      arr[i] = URL_BASE + arr[i].toUpperCase() + URL_CHART_SPEC;
+    }
+
+    return arr;
   }
 
 });
