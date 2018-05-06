9e7d7c5f3b14f9d09bdde7ceb8afbd4b87536353
Pratik Shah <pratik.t.shah@gmail.com>
2018-05-05 22:11:35 -0500

    Split and combine string as comma seperated

--- _site/tradelytics/apps/snowball/csvc.js
+++ _site/tradelytics/apps/snowball/csvc.js
@@ -1,5 +1,14 @@
 $(function() {
-  $("#box1").on("keyup", function() {
-    
+  $box1 = $("#box1");
+  $box2 = $("#box2");
+
+  $box1.on("keyup", function() {
+    var txt = $box1.val();
+    $box2.val(textToCSV(txt));
   });
 });
+
+// split array on every new line and then join by comma
+function textToCSV(text) {
+  return text.split("\n").join(",");
+}
