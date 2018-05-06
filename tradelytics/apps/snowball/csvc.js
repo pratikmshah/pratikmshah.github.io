81e6018807de1e0247d592421aadf4d30dada45e
Pratik Shah <pratik.t.shah@gmail.com>
2018-05-05 21:38:16 -0500

    Add csv convert html code

--- tradelytics/apps/snowball/csvc.js
+++ tradelytics/apps/snowball/csvc.js
@@ -1,26 +1,5 @@
-$(function () {
-
-    // focus and highlight first text box
-    $('textarea, input[type="text"]').first().focus().select();
-
-    $("#box1").keyup(function () {
-        process(false);
-    });
-
-
-    $("#box1").on('paste', function () {
-        setTimeout(function () {
-            process(true);
-        }, 1);
-    });
-
-
-    function process(pasted) {
-        var input = $('#box1').val();
-
-        if (typeof convert === 'function') {
-            $('#box2').val(convert(input, pasted));
-        }
-    }
-
+$(function() {
+  $("#box1").on("keyup", function() {
+    
+  });
 });
