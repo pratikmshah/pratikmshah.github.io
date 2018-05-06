7d93f1281f4bde0e028eff194f4da2c6e4a235fd
Pratik Shah <pratik.t.shah@gmail.com>
2018-05-05 22:33:52 -0500

    Add clear button to clear CSV boxes

--- _site/tradelytics/apps/snowball/csvc.js
+++ _site/tradelytics/apps/snowball/csvc.js
@@ -2,13 +2,34 @@ $(function() {
   $box1 = $("#box1");
   $box2 = $("#box2");
 
+  // convert string from box1 and place into box2
   $box1.on("keyup", function() {
     var txt = $box1.val();
     $box2.val(textToCSV(txt));
   });
+
+  // clear contents in box1 & 2
+  $("#clear-boxes").on('click', function() {
+    clearBoxes($box1, $box2);
+  });
+
+  // copy text from box2
+  $("copy-box").on('click', function() {
+    copyText();
+  });
+
 });
 
 // split array on every new line and then join by comma
 function textToCSV(text) {
   return text.split("\n").join(",");
+}
+
+function clearBoxes(one, two) {
+  one.val("");
+  two.val("");
+}
+
+function copyText() {
+
 }
