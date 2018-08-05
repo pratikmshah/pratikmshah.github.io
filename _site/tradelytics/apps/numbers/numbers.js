467b8131131cc4357333da12caf53eea8c5be458
Pratik Shah <pratik.t.shah@gmail.com>
2018-08-05 17:03:59 -0500

    Add google search function

--- _site/tradelytics/apps/numbers/numbers.js
+++ _site/tradelytics/apps/numbers/numbers.js
@@ -9,11 +9,24 @@ $(function() {
     $("#stock-chart img").attr("src", cLink);
   });
 
+  // google search button
+  $("#google-btn").on('click', function() {
+    if ($("#google-search").val()) {
+      googleSearch( $("#google-search").val() );
+    }
+  });
+
 });
 
 
 // get chart from finviz
-
 function getFinvizChart(ticker) {
   return "https://finviz.com/chart.ashx?t=" + ticker + "&ty=c&ta=1&p=d&s=l"
+}
+
+// google search
+function googleSearch(str) {
+  var GOOGLE_URL = "https://www.google.com/search?q="
+  var url = GOOGLE_URL + str.split(" ").join("+");
+  window.open(url);
 }
