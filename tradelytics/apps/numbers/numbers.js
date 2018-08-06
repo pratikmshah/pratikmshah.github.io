0c9e41bac29856e8c7f91f1518eb0c699447569c
Pratik Shah <pratik.t.shah@gmail.com>
2018-08-05 21:37:35 -0500

    Add dynamic links for each analysis section

--- tradelytics/apps/numbers/numbers.js
+++ tradelytics/apps/numbers/numbers.js
@@ -1,12 +1,20 @@
 var ticker;
+var financialLinks = {};
+var ratioLinks = {};
+var insiderLinks = {};
+var ownershipLinks = {};
+var statsLinks = {};
+var socialLinks = {};
 
 $(function() {
 
   // get chart after keypress in ticker input
+  // update the website links
   $('#ticker').on('keyup', function() {
     ticker = $('#ticker').val();
     var cLink = getFinvizChart(ticker);
     $("#stock-chart img").attr("src", cLink);
+    updateLinks();
   });
 
   // google search button
@@ -15,6 +23,9 @@ $(function() {
       googleSearch( $("#google-search").val() );
     }
   });
+
+  // social media search
+  
 
 });
 
@@ -29,4 +40,43 @@ function googleSearch(str) {
   var GOOGLE_URL = "https://www.google.com/search?q="
   var url = GOOGLE_URL + str.split(" ").join("+");
   window.open(url);
+}
+
+
+// function update links
+function updateLinks() {
+  financialLinks = {
+    marketwatchFin: "https://www.marketwatch.com/investing/stock/" + ticker +"/financials",
+    yahooFin: "https://finance.yahoo.com/quote/" + ticker + "/financials?",
+    nasdaqFin: "https://www.nasdaq.com/symbol/" + ticker + "/financials?",
+    secFin: "https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + ticker
+  };
+
+  ratioLinks = {
+    marketwatchRatio: "https://www.marketwatch.com/investing/stock/" + ticker + "/profile",
+    reutersRatio: "https://www.reuters.com/finance/stocks/financial-highlights/" + ticker
+  };
+
+  insiderLinks = {
+    marketwatchOfficer: "https://www.marketwatch.com/investing/stock/" + ticker + "/insideractions",
+    reutersOfficer: "https://www.reuters.com/finance/stocks/company-officers/" + ticker
+  };
+
+  ownershipLinks = {
+    marketwatchInsider: "https://www.marketwatch.com/investing/stock/" + ticker + "/insideractions",
+    finvizInsider: "https://finviz.com/quote.ashx?t=" + ticker,
+    nasdaqInsider: "https://www.nasdaq.com/symbol/" + ticker + "/ownership-summary"
+  };
+
+  statsLinks = {
+    finvizStats: "https://finviz.com/quote.ashx?t=" + ticker,
+    marketwatchStats: "https://www.marketwatch.com/investing/stock/" + ticker,
+    earningsStats: "https://earningscast.com/companies/" + ticker
+  };
+}
+
+updateSocialLinks() {
+  socialLinks = {
+    twitterSocial: "https://twitter.com/search?q="
+  }
 }
